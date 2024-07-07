dd4f7b115fc9c21a85f917e4c9c05e623ea198e8
Alejandro <151791940+blacky1412@users.noreply.github.com>
2024-07-06 19:49:31 -0500

    Update template.py

--- src/template.py
+++ src/template.py
@@ -104,8 +104,8 @@ with tab3:
     """
     )
     st.image("src/causalforest.png") # uncomment this line if you would like to add an image
-    #table = pd.read_csv("<<path/to/table.csv>>") # load a table from csv to show it on the page
-    #st.table(table) # show table
+    table = pd.read_csv("src/het_doublelasso.csv") # load a table from csv to show it on the page
+    st.table(table) # show table
 
 with tab4:
 

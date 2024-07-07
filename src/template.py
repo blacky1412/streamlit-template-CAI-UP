862cc3c2f9cfa5205a8794974d54db4f269c1e0d
Alejandro <151791940+blacky1412@users.noreply.github.com>
2024-07-06 19:25:51 -0500

    Update template.py

--- src/template.py
+++ src/template.py
@@ -56,7 +56,7 @@ with tab3:
 with tab4:
 
     st.markdown("""
-    <<Your description here, in Markdown>>
+    Wilson E, Free C, Morris TP, Syred J, Ahamed I, Menon-Johansson AS, et al. (2017) Internet-accessed sexually transmitted infection (eSTI) testing and results service: A randomised, single-blind, controlled trial. PLoS Med 14(12): e1002479. https://doi.org/10.1371/journal.pmed.1002479
     """
     )
     # st.image("<<path to image from project's root, if needed>>") # uncomment this line if you would like to add an image
